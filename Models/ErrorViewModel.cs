400300fbdfc3f8e2d338fd50c154ab1d8b03224e
Dorian Orozco <146119349+Dorian-Orozco@users.noreply.github.com>
2024-03-27 01:10:41 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GetOnIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
