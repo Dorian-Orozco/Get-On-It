afa4b4fc9c0fabe82b4de205de55578c11722439
Dorian Orozco <146119349+Dorian-Orozco@users.noreply.github.com>
2024-03-27 02:03:59 -0400

    Confirm Email, and other simple email services should be working.

---
+++ Services/SendEmail.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace GetOnIt.Services
+{
+    public class SendEmail
+    {
+        //Simple Task to Create and Pass back and email to send, public since it needs to be accessed in many places
+        public async Task<bool> SendEmailAsync(string email, string subject, string confirmLink)
+        {
+            try //try to set up the email, if fail return false.
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtpClient = new SmtpClient();
+                message.From = new MailAddress("dorianCodeDemo@outlook.com");
+                message.To.Add(email);
+                message.Subject = subject;
+                message.IsBodyHtml = true;
+                message.Body = confirmLink;
+
+                // Set up port and host name 
+                smtpClient.Port = 587;
+                smtpClient.Host = "smtp-mail.outlook.com";
+
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential("dorianCodeDemo@outlook.com", "nairoD4002!"); //please dont steal my account :(
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                smtpClient.Send(message);
+            }
+
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
